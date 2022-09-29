e1a67121ad3cb13756142f57faa6266efdcae0a7
Oleksiy <andriyshabo@gmail.com>
2022-09-29 14:34:50 +0300

    Correct syntax

--- src/Car.java
+++ src/Car.java
@@ -28,12 +28,24 @@ public class Car {
     public int getRegistrationNumber() {
         return registrationNumber;
     }
-    public void setId(int id) { this.id = id; }
-    public void setBrand(String brand) { this.brand = brand; }
-    public void setModel(String model) { this.model = model; }
-    public void setYear(int year) { this.year = year; }
-    public void setColor(String color) { this.color = color; }
-    public void setPrice(BigDecimal price) { this.price = price; }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
     public void setRegistrationNumber(int registrationNumber) {
         this.registrationNumber = registrationNumber;
     }
